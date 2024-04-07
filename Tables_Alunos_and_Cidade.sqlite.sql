2498ada2389ac64b10bc9601298b239141a3f54f
Isabela Queiroz <124175141+IsabelaQu@users.noreply.github.com>
2024-04-07 19:54:52 -0300

    Tabelas Alunos e Cidade

---
+++ Tables_Alunos_and_Cidade.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE Alunos (
+   id 			int PRIMARY KEY,
+   nome 		varchar(60) not NULL,
+   data_nasc	date,
+   id_cidade	int,
+   FOREIGN KEY	(id_cidade)	REFERENCES Cidade(id)
+  );
+CREATE TABLE Cidades ( 
+  id		int PRIMARY KEY,
+  nome 		varchar(50) NOT NULL,
+  populacao	int 
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
